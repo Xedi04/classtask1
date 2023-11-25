b76ee8368cc92601fd021c78f26ebb4dbb2889a4
FatimaRustamova <frustemova34@gmail.com>
2023-11-25 11:39:00 +0400

    unitask1

---
+++ script.js
@@ -0,0 +1,58 @@
+// let Btn=document.querySelector("#btn");
+
+
+// Btn.addEventListener("click", ()=>{
+
+//     let Div=document.createElement("Div");
+// document.body.append(Div);
+
+// })
+// let Btn = document.querySelector("#btn");
+
+// let array = [{ name: "fatima", age: 19 }, { name: "amaliya", age: 20 }]
+
+
+
+// Btn.addEventListener("click", () =>{
+//     let ul = document.createElement("ul");
+
+ 
+  
+
+//     for (let i = 0; i <= array.length; i++){
+//         let li = document.createElement("li");
+//         li.innerText = array[i].name + "-" + array[i].age;
+
+//          ul.append(li)
+//     }
+//     document.body.append(ul)
+// })
+
+// let Btn = document.querySelector("#btn");
+// let ad=document.querySelector("#ad");
+// let sad=document.querySelector("#sad");
+// let body=document.querySelector("body");
+
+
+// Btn.addEventListener("click", () => {
+
+// let div=document.createElement("div");
+// body.append(div);
+// div.append(ad.value + " " + sad.value);
+
+// })
+
+
+// let nav=document.querySelector("#nav");
+
+
+
+let body=document.querySelector("body");
+let r1=document.createElement("input");
+let r2=document.createElement("input");
+let r3=document.createElement("input");
+
+
+
+
+
